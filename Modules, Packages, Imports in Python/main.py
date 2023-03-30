c9aa2d54377bcc978e19f517c0031c75dd215d9a
Lyota <korystin.aleksei@yandex.ru>
2023-03-30 22:05:40 +0300

    file update

--- Modules, Packages, Imports in Python/main.py
+++ Modules, Packages, Imports in Python/main.py
@@ -3,12 +3,6 @@ from application.application.db.people import get_employees
 from datetime import date
 from textual_datepicker import DateSelect
 
-DateSelect(
-  placeholder="please select",
-  format="YYYY-MM-DD",
-  picker_mount="#main_container"
-)
-
 def show_date():
     t = date.today()
     print(t.strftime('Дата: %d.%m.%Y'))
