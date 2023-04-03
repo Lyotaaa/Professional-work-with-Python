4d181fe877e85a60961cc417f68f4843c53addc2
Lyotaaa <korystin.aleksei@yandex.ru>
2023-04-03 12:58:47 +0300

    update

--- 2. Regular expressions/main.py
+++ 2. Regular expressions/main.py
@@ -1,7 +1,13 @@
 from pprint import pprint
+import os
 import csv
+
+current = os.getcwd()
+folder_name = '2. Regular expressions'
+file_name_r = 'phonebook_raw.csv'
+full_path_r = os.path.join(current, folder_name, file_name_r)
 # Читаем адресную книгу в формате CSV в список contacts_list
-with open("phonebook_raw.csv", encoding="utf8") as f:
+with open(full_path_r, encoding="utf8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
 pprint(contacts_list)
@@ -9,8 +15,10 @@ pprint(contacts_list)
 # Ваш код.
 
 # 2. Сохраните получившиеся данные в другой файл.
+file_name_w = 'phonebook.csv'
+full_path_w = os.path.join(current, folder_name, file_name_w)
 # Код для записи файла в формате CSV:
-with open("phonebook.csv", "w", encoding="utf8") as f:
+with open(full_path_w, "w", encoding="utf8") as f:
     datawriter = csv.writer(f, delimiter=',')
 # Вместо contacts_list подставьте свой список:
     datawriter.writerows(contacts_list)
