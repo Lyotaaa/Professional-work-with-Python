ee2e6f56c07bf24159ef96ecbfafc76e1c26c1f5
Lyota <korystin.aleksei@yandex.ru>
2023-04-03 22:58:56 +0300

    update

--- 2. Regular expressions/main.py
+++ 2. Regular expressions/main.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import os
 import csv
+import re
 
 current = os.getcwd()
 folder_name = '2. Regular expressions'
@@ -10,15 +11,20 @@ full_path_r = os.path.join(current, folder_name, file_name_r)
 with open(full_path_r, encoding="utf8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-pprint(contacts_list)
+# pprint(contacts_list)
 # 1.  Выполните пункты 1-3 задания.
-# Ваш код.
+for pers_info in contacts_list:
+    res = " ".join(pers_info)
+    print(res)
+    
+    # word_list = re.findall(r"\w+", contacts_list)
+    # print(contacts_list)
 
 # 2. Сохраните получившиеся данные в другой файл.
-file_name_w = 'phonebook.csv'
-full_path_w = os.path.join(current, folder_name, file_name_w)
+# file_name_w = 'phonebook.csv'
+# full_path_w = os.path.join(current, folder_name, file_name_w)
 # Код для записи файла в формате CSV:
-with open(full_path_w, "w", encoding="utf8") as f:
-    datawriter = csv.writer(f, delimiter=',')
-# Вместо contacts_list подставьте свой список:
-    datawriter.writerows(contacts_list)
+# with open(full_path_w, "w", encoding="utf8") as f:
+#     datawriter = csv.writer(f, delimiter=',')
+# # Вместо contacts_list подставьте свой список:
+#     datawriter.writerows(contacts_list)
