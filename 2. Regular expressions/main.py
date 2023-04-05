0364529dd7a5181eca5b01220bfd877d4a4d1426
Lyota <korystin.aleksei@yandex.ru>
2023-04-05 23:27:11 +0300

    update

--- 2. Regular expressions/main.py
+++ 2. Regular expressions/main.py
@@ -3,143 +3,67 @@ import os
 import csv
 from re import sub
 import re
+from textual_datepicker import DateSelect
 
-current = os.getcwd()
-folder_name = '2. Regular expressions'
-file_name_r = 'phonebook_raw.csv'
-full_path_r = os.path.join(current, folder_name, file_name_r)
-# Читаем адресную книгу в формате CSV в список contacts_list
-with open(full_path_r, encoding="utf8") as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
+def find_file(folder_name, file_name):
+    current = os.getcwd()
+    full_path = os.path.join(current, folder_name, file_name)
+    return full_path
 
-pattern = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)' \
-        r'(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)' \
-        r'(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
-new_phone = r'+7(\4)\8-\11-\14\15\17\18\19\20'
+def open_file(full_path):
+    with open(full_path, encoding="utf8") as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
+    return contacts_list
 
-res_list = []
-for i in contacts_list:
-    # print(i)
-    res = (",".join(i))
-    res = re.sub(pattern, new_phone, res)
-    res = res.split(',')
-    res_list.append(res)
-# print(res_list)
-total ={}
-r = res_list[2:]
-# pprint(r)
-result = {}
-for name, l_name, s_name, *rest in r:
-    info = name + ' ' +l_name + ' ' + s_name
-    print(info)
-    # result.setdefault(tuple(name, l_name, s_name), []).append(rest) 
-# print(result)
-# for i in range(len(r)):
-    # print(i[0])
-    
+def pattern():
+    pattern = r"(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(\-*)(\d{3})(\s*)(\-*)" \
+        r"(\d{2})(\s*)(\-*)(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)"
+    new_phone = r'+7(\4)\8-\11-\14\15\17\18\20'
+    return [pattern, new_phone]
 
+def change_phone(contacts_list):
+    res_list = []
+    for i in contacts_list:
+        res = (",".join(i))
+        res = re.sub(pattern()[0], pattern()[1], res)
+        res = res.split(',')
+        res_list.append(res)
+    return res_list 
 
-    # if len(r[i][0].split()) == 3:
-        # print(r[i][0])
-    #     total[i + 1] = {
-    #         'lastname': (r[i][0].split())[0],
-    #         'firstname': (r[i][0].split())[1],
-    #         'surname': (r[i][0].split())[2],
-    #         'organization': r[i][3],
-    #         'position': r[i][4],
-    #         'phone': r[i][5],
-    #         'email':r[i][6]
-    #     }
-    # elif len(r[i][0].split()) == 2 and r[i][0][1] == None:
-    #     total[i + 1] = {
-    #         'lastname': (r[i][0].split())[0],
-    #         'firstname': (r[i][0].split())[1],
-    #         'surname': r[i][2],
-    #         'organization': r[i][3],
-    #         'position': r[i][4],
-    #         'phone': r[i][5],
-    #         'email':r[i][6]
-    #     }
-    # elif len(r[i][0].split()) == 1 and r[i][]:
-    #     total[i + 1] = {
-    #         'lastname': (r[i][0].split())[0],
-    #         'firstname': r[i][1],
-    #         'surname': r[i][2],
-    #         'organization': r[i][3],
-    #         'position': r[i][4],
-    #         'phone': r[i][5],
-    #         'email':r[i][6]
-    #     }
-# pprint(total)
-'''print(res_list)
-total = {}
-for i in res_list[1:]:
-    # print(i[0])
-    if len(i[0].split()) == 3:
-        total[i[0]] = []
-    if len(i[0].split()) == 2:
-        total[i[0] + i[1]] = []'''
+def view_normalization(res_list):
+    normal_view = [' '.join(i[0:3]).split(' ')[0:3] + i[3:7] for i in res_list]
+    return normal_view[1:]
 
-# pprint(total)        
+def save_contacts_list(normal_view):
+    finish_list = []
+    for more in normal_view:
+        for basic in normal_view:
+            if more[0:2] == basic[0:2]:
+                basic_list = more
+                more = basic_list[0:2]
+                for i in range(2, 7):
+                    if basic_list[i] == '':
+                        more.append(basic[i])
+                    else:
+                        more.append(basic_list[i])
+        if more not in finish_list:
+            finish_list.append(more)
+    return finish_list
+
+def write_file(folder_name, file_name, finish_list):
+    current = os.getcwd()
+    full_path = os.path.join(current, folder_name, file_name)
+    with open(full_path, "w", encoding="utf8") as f:
+        datawriter = csv.writer(f, delimiter='|')
+        datawriter.writerows(finish_list)
 
 
-# for i in range(len(res_list)):
-#     for i in range(len(res_list[i])):
-
-
-
-
-
-# def change_phone(contacts_list):
-#     pattern = r"(8|\+7)\s*\(*(\d{3})\)*[-\s]*(\d{3})[-\s]*(\d{2})[-\s]*(\d{2})\s*[\(доб.\s]*(\d{4})*\)*"
-#     repl_extra = r"+7(\2)\3-\4-\5 доб.\6"
-#     repl = r"+7(\2)\3-\4-\5"
-#     for i in contacts_list:
-#         if "доб" not in i:
-#             i = re.sub(pattern, repl, i)
-#         elif "доб" in i:
-#             i[5] = re.sub(pattern, repl_extra, i)
-#         print(i)
-# change_phone(contacts_list)
-
-# pprint(contacts_list)
-# 1.  Выполните пункты 1-3 задания.
-
-# information_list = []
-    # word_list = re.findall(r"\w+", contacts_list)
-    # print(contacts_list)
-
-# 2. Сохраните получившиеся данные в другой файл.
-# file_name_w = 'phonebook.csv'
-# full_path_w = os.path.join(current, folder_name, file_name_w)
-# Код для записи файла в формате CSV:
-# with open(full_path_w, "w", encoding="utf8") as f:
-#     datawriter = csv.writer(f, delimiter=',')
-# # Вместо contacts_list подставьте свой список:
-#     datawriter.writerows(contacts_list)
-
-# num_pattern = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)' \
-#                   r'(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)' \
-#                   r'(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
-    
-# num_pattern_new = r'+7(\4)\8-\11-\14\15\17\18\20'
-# contacts_list_new = list()
-# for page in contacts_list:
-#   page_string = ','.join(page) # объединение в строку
-#   format_page = sub(num_pattern, num_pattern_new, page_string) # замена шаблонов в строке
-#   page_list = format_page.split(',') # формируем список строк
-#   contacts_list_new.append(page_list)
-# print(contacts_list_new[2:4])
-
-
-pattern = r'(\+7|8)+[\s(]*(\d{3,3})[\s)-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})[\s]*[(доб.\s]*(\d+)[)]*'
-# pattern = r"(8|\+7)\s*\(*(\d{3})\)*[-\s]*(\d{3})[-\s]*(\d{2})[-\s]*(\d{2})\s*[\(доб.\s]*(\d{4})*\)*"
-# 
-repl_extra = r"+7(\2)\3-\4-\5 доб.\6 "
-repl = r"+7(\2)\3-\4-\5 "
-# for i in res_list[2:]:
-#     if "доб." not in res_list[2:]:
-#         res_list = re.sub(pattern, repl, res_list)
-#     elif "доб." in res_list:
-#         res_list = re.sub(pattern, repl_extra, res_list)
+if __name__ == '__main__':
+    file = find_file('2. Regular expressions', 'phonebook_raw.csv')
+    contacts_list = open_file(file)
+    normal_phone = change_phone(contacts_list)
+    normal_view = view_normalization(normal_phone)
+    finish_list = save_contacts_list(normal_view)
+    # print(*finish_list,sep="\n")
+    write_file('2. Regular expressions', 'phonebook.csv', finish_list)
