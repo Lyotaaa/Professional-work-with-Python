9a843698a6d867d40a8a92b5c79838472bb3f29a
Lyota <korystin.aleksei@yandex.ru>
2023-04-10 00:17:06 +0300

    add main

---
+++ 2. Iterators. Generators. Yield/main.py
@@ -0,0 +1,11 @@
+from Task_1 import test_1
+from Task_2 import test_2
+from Task_3 import test_3
+from Task_4 import test_4
+
+if __name__ == "__main__":
+    test_1()
+    test_2()
+    test_4()
+    """И тут я зашёл в тупик! Подскажите, что не так."""
+    test_3()
