71fc719983e4285e4aeeff87d5030c802ea9eb68
Lyota <korystin.aleksei@yandex.ru>
2023-04-12 17:53:02 +0300

    update Task 2

--- 3. Decorators/main.py
+++ 3. Decorators/main.py
@@ -1,7 +1,8 @@
 from Task_1.Task_1 import test_1
+from Task_2.Task_2 import test_2
 
-# from Task_2 import test_2
 # from Task_3 import test_3
 
 if __name__ == "__main__":
-    test_1()
+    # test_1()
+    test_2()
