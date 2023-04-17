ed0c73e3518bffa6e12fc8375397613ed489569b
Lyota <korystin.aleksei@yandex.ru>
2023-04-17 21:27:17 +0300

    update test_yandex

--- 4. Tests/src/yandex.py
+++ 4. Tests/src/yandex.py
@@ -1,7 +1,6 @@
 import requests
 from configparser import ConfigParser
 import json
-from pprint import pprint
 import sys
 
 
@@ -12,52 +11,53 @@ def open_a_token(file_name):
     return token
 
 
+def yandex_info():
+    res_YA = YandexDisk(open_a_token("confing.ini"))
+    return res_YA
+
+
 class YandexDisk:
-    def __init__(self, folder_name, token):
+    def __init__(self, token):
         self.token = token
-        self.folder_name = folder_name
         self.url_resources = "https://cloud-api.yandex.net/v1/disk/resources"
         self.headers = {"Authorization": self.token}
-        self.params = {"path": self.folder_name}
 
-    def get_folder(self):
+    def get_folder(self, folder_name):
         response = requests.get(
-            url=self.url_resources, headers=self.headers, params=self.params
+            url=self.url_resources, headers=self.headers, params={"path": folder_name}
         )
         if response.status_code == 200:
             # json_text = json.loads(response.text)
             return True, response.status_code
         elif response.status_code != 200:
-            return False, self.folder_name
+            return False, folder_name
 
-    def create_a_folder(self):
-        respons_get_folder = self.get_folder()
+    def create_a_folder(self, folder_name):
+        respons_get_folder = self.get_folder(folder_name)
         if respons_get_folder[0]:
-            return False, self.folder_name
+            return False, folder_name
         else:
             response = requests.put(
-                url=self.url_resources, headers=self.headers, params=self.params
+                url=self.url_resources,
+                headers=self.headers,
+                params={"path": folder_name},
             )
             return True, response.status_code
 
-    def delete_folder(self):
-        respons_get_folder = self.get_folder()
+    def delete_folder(self, folder_name):
+        respons_get_folder = self.get_folder(folder_name)
         if respons_get_folder[0]:
             response = requests.delete(
-                url=self.url_resources, headers=self.headers, params=self.params
+                url=self.url_resources,
+                headers=self.headers,
+                params={"path": folder_name},
             )
             return True, response.status_code
         else:
-            return False, self.folder_name
+            return False, folder_name
 
 
 if __name__ == "__main__":
-
-    def yandex_info():
-        folder_name = input("Введите назание папки: ")
-        res_YA = YandexDisk(folder_name, open_a_token("confing.ini"))
-        return res_YA
-
     print(
         """Список команд:
     gf – Информация о папке.
@@ -68,22 +68,25 @@ if __name__ == "__main__":
     while True:
         command = input("Ведите команду: ")
         if command == "gf":
+            folder_name = input("Введите назание папки: ")
             res_YA = yandex_info()
-            result = res_YA.get_folder()
+            result = res_YA.get_folder(folder_name)
             if result[0]:
                 print("Такая папка уже есть. Статус код: {}".format(result[1]))
             else:
                 print("Папки с именем {} нет.".format(result[1]))
         elif command == "cr":
+            folder_name = input("Введите назание папки: ")
             res_YA = yandex_info()
-            result = res_YA.create_a_folder()
+            result = res_YA.create_a_folder(folder_name)
             if result[0]:
                 print("Папка успешно создана. Статус код: {}".format(result[1]))
             else:
                 print("Папка с именем {} уже существует.".format(result[1]))
         elif command == "df":
+            folder_name = input("Введите назание папки: ")
             res_YA = yandex_info()
-            result = res_YA.delete_folder()
+            result = res_YA.delete_folder(folder_name)
             if result[0]:
                 print("Папка успешно удалена. Статус код: {}".format(result[1]))
             else:
---
+++ 4. Tests/tests/test_yandex.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from parameterized import parameterized
+import pytest
+from src.yandex import YandexDisk, yandex_info
+
+res_YA = yandex_info()
+
+
+class Test_Add_Unittest(TestCase):
+    def test_get_folder(self):
+        expected_1 = (False, "1")
+        expected_2 = (True, 200)
+        self.assertEqual(res_YA.get_folder("1"), expected_1)
+        self.assertEqual(res_YA.get_folder("!Test"), expected_2)
+
+    def test_create_a_folder(self):
+        expected_1 = (False, "1")
+        expected_2 = (True, 201)
+        self.assertEqual(res_YA.create_a_folder("1"), expected_2)
+        self.assertEqual(res_YA.create_a_folder("1"), expected_1)
+
+    def test_ddelete_folder(self):
+        expected_1 = (False, "1")
+        expected_2 = (True, 204)
+        self.assertEqual(res_YA.delete_folder("1"), expected_2)
+        self.assertEqual(res_YA.delete_folder("1"), expected_1)
+
+
+class Test_Yandex_Pytest:
+    def test_get_folder(self):
+        assert res_YA.get_folder("1") == (False, "1")
+        assert res_YA.get_folder("test") == (True, 200)
+
+    def test_create_a_folder(self):
+        assert res_YA.create_a_folder("1") == (True, 201)
+        assert res_YA.create_a_folder("1") == (False, "1")
+
+    def test_delete_folder(self):
+        assert res_YA.delete_folder("test") == (True, 204)
+        assert res_YA.delete_folder("2") == (False, "2")
