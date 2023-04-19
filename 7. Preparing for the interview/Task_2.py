70456c530bc87a6dfd193c0eeaa24de21b15eb20
Lyota <korystin.aleksei@yandex.ru>
2023-04-19 22:40:03 +0300

    update Task 2

--- 7. Preparing for the interview/Task_2.py
+++ 7. Preparing for the interview/Task_2.py
@@ -2,7 +2,7 @@ from Task_1 import Stack
 
 noraml_list = ["(((([{}]))))", "[([])((([[[]]])))]{()}", "{{[()]}}"]
 abnormal_list = ["}{}", "{{[(])]}}", "[[{())}]"]
-
+check_tuple = ("()", "[]", "{}")
 
 def check_string(query):
     stack = Stack()
@@ -13,22 +13,29 @@ def check_string(query):
             if i in "([{":
                 stack.push(i)
             elif i not in "([{":
-                if stack.peek() == "(" and i == ")":
-                    stack.pop()
-                elif stack.peek() == "[" and i == "]":
-                    stack.pop()
-                elif stack.peek() == "{" and i == "}":
+                # Вариант 1
+                if stack.peek() + i in check_tuple:
                     stack.pop()
                 else:
                     return False
-            else:
-                return False
     return True
+                # Вариант 2
+    #             if stack.peek() == "(" and i == ")":
+    #                 stack.pop()
+    #             elif stack.peek() == "[" and i == "]":
+    #                 stack.pop()
+    #             elif stack.peek() == "{" and i == "}":
+    #                 stack.pop()
+    #             else:
+    #                 return False
+    #         else:
+    #             return False
+    # return True
 
 
 if __name__ == "__main__":
     for query in noraml_list + abnormal_list:
-        if check_string(query):
+        if check_string(query) and Stack().is_empty():
             print(f"{query:<22} | Сбалансировано")
         else:
             print(f"{query:<22} | Несбалансировано")
