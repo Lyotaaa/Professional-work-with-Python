5476cf7ae164e2a0ffd0e12b3e7484ea9db58f5c
Lyota <korystin.aleksei@yandex.ru>
2023-04-20 20:45:55 +0300

    Update Task 3

--- 7. Preparing for the interview/Task_3.py
+++ 7. Preparing for the interview/Task_3.py
@@ -3,7 +3,7 @@ import smtplib
 import imaplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
+from email.header import decode_header
 
 class Email:
     def __init__(self, login, password):
@@ -16,7 +16,7 @@ class Email:
         send_message["To"] = ", ".join(recipients)
         send_message["Subject"] = subject
         send_message.attach(MIMEText(message))
-        server = smtplib.SMTP(protocol + ": " + str(587))
+        server = smtplib.SMTP(protocol, 587)
         server.ehlo()
         server.starttls()
         server.ehlo()
@@ -25,35 +25,21 @@ class Email:
             send_message["From"], send_message["To"], send_message.as_string()
         )
         server.quit()
-        return result
+        return "send end"
 
     def recieve_an_email(self, protocol, header=None):
         login_email = imaplib.IMAP4_SSL(protocol)
-        login_email = login(self.login, self.password)
+        login_email.login(self.login, self.password)
         login_email.list()
         login_email.select("inbox")
         criterion = '(HEADER Subject "%s")' % header if header else "ALL"
         result, data = login_email.uid("search", None, criterion)
         assert data[0], "There are no letters with current header"
         latest_email_uid = data[0].split()[-1]
-        result, data = login_email.uid("fetch", latest_email_uid, "(RFC822)")
+        result, data = login_email.uid("fetch", latest_email_uid.decode("UTF-8"), "(RFC822)")
         raw_email = data[0][1]
-        result_query = email.message_from_string(raw_email)
+        result_query = email.message_from_string(raw_email.decode("UTF-8"))
+        # Дополнительная срока для проверки
+        result = decode_header(result_query["Subject"])[0][0].decode()
         login_email.logout()
-        return result_query
-
-
-if __name__ == "__main__":
-    login = "login@gmail.com"
-    password = "qwerty"
-    gmail = Email(login, password)
-    recipients = ["vasya@email.com", "petya@email.com"]
-    subject = "Subject"
-    message = "Message"
-    protocol_send = "smtp.gmail.com"
-    send_message = gmail.send_message_to_email(
-        recipients, subject, message, protocol_send
-    )
-    protocol_recieve = "imap.gmail.com"
-    header = None
-    recieve_message = gmail.recieve_an_email(protocol_recieve, header)
+        return [result, "recieve end"]
---
+++ 7. Preparing for the interview/main.py
@@ -0,0 +1,28 @@
+from configparser import ConfigParser
+from Task_3 import Email
+from pprint import pprint
+
+def open_a_token(file_name):
+    cofing = ConfigParser()
+    cofing.read(file_name)
+    login = cofing["token_info"]["login"]
+    password = cofing["token_info"]["password"]
+    recipients = cofing["token_info"]["recipients"]
+    return login, password, recipients.split(",")
+
+if __name__ == "__main__":
+    login_info = open_a_token("confing.ini")
+    login = login_info[0]
+    password = login_info[1]
+    gmail = Email(login, password)
+    recipients = login_info[2]
+    subject = "LOL"
+    message = "KEK"
+    protocol_send = "smtp.gmail.com"
+    send_message = gmail.send_message_to_email(
+        recipients, subject, message, protocol_send
+    )
+    print(send_message)
+    protocol_recieve = "imap.gmail.com"
+    recieve_message = gmail.recieve_an_email(protocol_recieve)
+    print(*recieve_message, sep="\n")
